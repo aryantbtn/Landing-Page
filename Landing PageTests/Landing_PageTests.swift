b70da171e9c887386731ec7782fdb765545262c4
Aryan Singhal <aryan.22scse1180055@galgotiasuniversity.edu.in>
2024-12-02 11:07:49 +0530

    Initial Commit

---
+++ Landing PageTests/Landing_PageTests.swift
@@ -0,0 +1,17 @@
+//
+//  Landing_PageTests.swift
+//  Landing PageTests
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import Testing
+@testable import Landing_Page
+
+struct Landing_PageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
